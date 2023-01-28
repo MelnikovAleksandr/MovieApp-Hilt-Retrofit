c711588b24bc4c175a41861ddf1c4503b8e3cf10
Your Name <melnikov150990@mail.ru>
2023-01-28 16:41:26 +0500

    detailsFragment

--- app/src/main/java/ru/asmelnikov/android/movieapphiltretrofit/ui/MoviesFragment.kt
+++ app/src/main/java/ru/asmelnikov/android/movieapphiltretrofit/ui/MoviesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
@@ -57,6 +58,13 @@ class MoviesFragment : Fragment() {
                                     recyclerMovies.apply {
                                         layoutManager = LinearLayoutManager(requireContext())
                                         adapter = moviesAdapter
+                                    }
+                                    moviesAdapter.setOnItemClickListener {
+                                        val direction =
+                                            MoviesFragmentDirections.actionMoviesFragmentToMovieDetailsFragment(
+                                                it.id
+                                            )
+                                        findNavController().navigate(direction)
                                     }
                                 }
                             }
